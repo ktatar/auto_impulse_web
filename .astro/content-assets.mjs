6ca739945232fb8a01983ff4a089b5dcd3094ee1
Kivanc Tatar <kivanctatar@pm.me>
2026-07-28 23:22:38 +0200

    added exposing da

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_Z1IR5xe from "../../data/exposing-da.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fexposing-the-bias-in-ai-digital-assemblage.md";
+export default new Map([["../../data/exposing-da.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2Fexposing-the-bias-in-ai-digital-assemblage.md", __ASTRO_IMAGE_IMPORT_Z1IR5xe]]);
+		
